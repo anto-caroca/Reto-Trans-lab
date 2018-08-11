20f17084578714ff23781de0eb5f8e7be9663da6
antonella <antonella_caroca@hotmail.com>
2018-08-11 19:23:01 -0400

    agrego archivo menu.js y logout.js para no repetir código

--- src/js/tarifa.js
+++ src/js/tarifa.js
@@ -1,39 +1,4 @@
-//funcionalidad del side Menú
-function toggleMenu() { // añadir función onclick="toggleMenu()" al botón del nav bar y al botón cerrar.
-  if (sideMenu.className.indexOf("menu_closed") >= 0) { // primero revisamos si la clase d-none esta
-    openMenu();  // si esta la clase quiere decir que el menú esta cerrado, asi que llamamos la funcion para abrirlo
-  } else {
-    closeMenu(); //si no esta la clase, le indicamos que cierre el menu
-  }
-}
 
-function openMenu() {
-  sideMenu.classList.remove('menu_closed'); // quitando clase display-none
-  sideMenu.classList.add('menu_open');
-}
-
-function closeMenu() {
-  sideMenu.classList.add('menu_closed'); // añadimos la clase display-none
-  sideMenu.classList.remove('menu_open');
-}
-
-function logoutWithFirebase(){
-    firebase.auth().signOut()
-        .then(()=>{
-            console.log("Usuario finalizó su sesión");
-            window.location="index.html";
-        })
-        .catch((error)=>{
-            console.log("Error de firebase > Código > "+error.code); //error.code nos mostrará el código de error para informarnos qué pasó
-            console.log("Error de firebase > Mensaje > "+error.message); //error.message nos mostrará el mensaje de firebase del mismo error
-            // modal con mensaje de error
-            $('#myModal').modal();
-            let msn = document.getElementById("modalP");
-           if (error.code) {
-            msn.innerHTML = error.message;
-           }
-        });
-  }
 
 const renderBipBalance = document.getElementById("renderBipBalance"); 
 let storeBipBalance;
